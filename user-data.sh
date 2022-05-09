4fa0238cf670e2c8d1dbf97a096b186a7c98760f
Jacob Brinlee <66123601+jrbrinlee1@users.noreply.github.com>
2022-05-09 17:16:54 -0500

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# grafana specifics 
+# grafana specs 
 GRAF_VERSION=8.4.3-1
 GRAF_ARCH=aarch64
 GRAF_PACK=rpm
@@ -9,10 +9,14 @@ GRAF_PACK=rpm
 FB_USERNAME=<FB_USERNAME>
 FB_PASSWORD=<FB_PASSWORD>
 
+# FeatureBase plugin specs
+FB_VERSION=4.8.0
+FB_PLUGIN_VERSION=2.2.3
+
 # install grafana and FB plugin
 wget -P /home/ec2-user/ https://dl.grafana.com/enterprise/release/grafana-enterprise-$GRAF_VERSION.$GRAF_ARCH.$GRAF_PACK
 sudo yum install -y /home/ec2-user/grafana-enterprise-$GRAF_VERSION.$GRAF_ARCH.$GRAF_PACK
-wget -P /home/ec2-user/ https://$FB_USERNAME:$FB_PASSWORD@internal-builds.molecula.cloud/grafana-plugin/main/_latest/molecula-grafana-plugin.zip
+wget -P /home/ec2-user/ https://$FB_USERNAME:$FB_PASSWORD@releases.molecula.cloud/molecula-v$FB_VERSION/grafana-plugin/molecula-grafana-plugin-v$FB_PLUGIN_VERSION.zip
 sudo unzip /home/ec2-user/molecula-grafana-plugin.zip -d /home/ec2-user/
 sudo cp -rf /home/ec2-user/molecula/dist /var/lib/grafana/plugins/
 sudo sed -i 's/;allow_loading_unsigned_plugins =/allow_loading_unsigned_plugins = molecula-datasource/' /etc/grafana/grafana.ini
